6f2abfaa321af12923013da1e78c547fdc7513c5
Michael Noonan <michaelnoonan@gmail.com>
2016-08-25 19:04:32 +1000

    Added some contract stability tests - let's see if it works.

---
+++ tests/OctoTFS.Tests/OctoTFS.Tests/ContractStabilityFixture.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace OctoTFS.Tests
+{
+    [TestFixture]
+    [UseReporter(typeof (DiffReporter))]
+    public class ContractStabilityFixture
+    {
+        [Test]
+        [Description("This test helps to make sure we don't accidentally change the name nor data type of inputs people have stored in VSTS - otherwise they will likely lose data.")]
+        public void EnsureInputNamesAndTypesHaveNotChanged()
+        {
+            var taskJsonResourceNames = Assembly.GetAssembly(GetType())
+                .GetManifestResourceNames()
+                .Where(name => name.EndsWith("task.json"))
+                .OrderBy(name => name)
+                .ToArray();
+
+            var toApprove = taskJsonResourceNames.Aggregate(new StringBuilder(), (builder, resourceName) =>
+            {
+                builder.AppendIndentedLine(resourceName);
+
+                using (var resourceStream = Assembly.GetAssembly(GetType()).GetManifestResourceStream(resourceName))
+                using (var textReader = new StreamReader(resourceStream))
+                using (var jsonReader = new JsonTextReader(textReader))
+                {
+                    dynamic task = JsonSerializer.CreateDefault().Deserialize(jsonReader);
+                    foreach (dynamic input in task.inputs)
+                    {
+                        builder.AppendIndentedLine($"'{input.name}': {input.type}", 1);
+                    }
+                }
+
+                return builder;
+            }, builder => builder.ToString());
+
+            Approvals.Verify(toApprove);
+        }
+    }
+
+    public static class StringBuilderExtensions
+    {
+        public static StringBuilder AppendIndentedLine(this StringBuilder builder, string append, int indent = 0)
+        {
+            return builder.Append(new string('\t', indent)).AppendLine(append);
+        }
+    }
+}
