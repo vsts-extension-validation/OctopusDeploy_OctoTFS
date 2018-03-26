46f36c633246260337b148fffd805d2aed73af35
Matthew Casperson <matthewcasperson@gmail.com>
2018-03-26 13:07:12 +1000

    Enabled Tls 1.1 and 1.2

--- source/VSTSExtensions/OctopusBuildAndReleaseTasks/Tasks/CreateOctopusRelease/Octopus-CreateRelease.ps1
+++ source/VSTSExtensions/OctopusBuildAndReleaseTasks/Tasks/CreateOctopusRelease/Octopus-CreateRelease.ps1
@@ -1,13 +1,6 @@
 ﻿[CmdletBinding()]
 param()
 
-# https://github.com/OctopusDeploy/Issues/issues/4341
-# Enable TLS 1.0, 1.1 and 1.2
-[Net.ServicePointManager]::SecurityProtocol = `
- 		[Net.SecurityProtocolType]::Tls12 -bor `
- 		[Net.SecurityProtocolType]::Tls11 -bor `
- 		[Net.SecurityProtocolType]::Tls10
-
 # Get release notes from linked changesets and work items
 function Get-LinkedReleaseNotes($vssEndpoint, $comments, $workItems) {
     Write-Host "Environment = $env:BUILD_REPOSITORY_PROVIDER"
@@ -128,6 +121,13 @@ function Get-ReleaseNotes($linkedItemReleaseNotes) {
 Trace-VstsEnteringInvocation $MyInvocation
 
 try {
+	# https://github.com/OctopusDeploy/Issues/issues/4341
+	# Enable TLS 1.0, 1.1 and 1.2, and SSL 3. The most secure option will be used by default.
+	[Net.ServicePointManager]::SecurityProtocol = `
+ 		[Net.SecurityProtocolType]::Tls12 -bor `
+ 		[Net.SecurityProtocolType]::Tls11 -bor `
+ 		[Net.SecurityProtocolType]::Tls -bor `
+ 		[Net.SecurityProtocolType]::Ssl3
 
     . .\Octopus-VSTS.ps1
 
