0fc2dbea3e7397eb3009038ae3f5d17495a92e06
Shannon Lewis <slewis74@gmail.com>
2018-10-25 14:29:26 +1000

    fixed parameter name in the call to pack.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -7,7 +7,7 @@ $shareWith = $OctopusParameters["ShareWith"]
 $publish = [System.Convert]::ToBoolean($OctopusParameters["Publish"])
 $embeddedOctoVersion = $OctopusParameters["EmbeddedOctoVersion"]
 
-& "$PSScriptRoot\pack.ps1" -environment $environment -version $version -octoVersion $embeddedOctoVersion
+& "$PSScriptRoot\pack.ps1" -environment $environment -version $version -embeddedOctoVersion $embeddedOctoVersion
 if ($publish) {
     & "$PSScriptRoot\publish.ps1" -environment $environment -version $version -accessToken $accessToken -shareWith $shareWith
 }
