6764bd99bae3add5d89b32ec111c59437d015da5
shaunmarx <shaun.m4rx@gmail.com>
2019-01-29 16:55:18 +1000

    Upgrade to gulp v4

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -1,44 +1,45 @@
 var gulp = require("gulp");
 var path = require("path");
-var run = require("gulp-run-command").default;
 var clean = require("gulp-clean");
-var runSequence = require("run-sequence");
 var paths = require("../paths");
 var glob = require("glob");
+var cp = require('child_process');
 const argv = require('yargs').argv
 
-gulp.task("build", (callback) => {
-    return runSequence("clean", ["build:tasks", "build:widgets", "build:copy", "build:copy:task:content"], callback);
+
+
+gulp.task("build:tasks", () => {
+    return cp.exec("./node_modules/.bin/webpack --mode=development --require ts-node/register --require tsconfig-paths/register",
+    {
+        env: {
+            TS_NODE_PROJECT: "build/tsconfig-webpack.json",
+            EXTENSION_VERSION: argv.extensionVersion
+        }
+    });
 });
 
-gulp.task("build:tasks", [], run("./node_modules/.bin/webpack --mode=development --require ts-node/register --require tsconfig-paths/register", {
-    env: {
-        TS_NODE_PROJECT: "build/tsconfig-webpack.json",
-        EXTENSION_VERSION: argv.extensionVersion
-    }
-}));
+gulp.task("build:widget:source", () => {
+    return gulp.src(`${paths.sourceRoot}widgets/**/*`, {base: `${paths.sourceRoot}widgets`})
+    .pipe(gulp.dest(`${paths.outputPath}widgets`));
+});
 
-gulp.task("build:widgets", ["build:widget:source"], () =>
+gulp.task("build:widgets", gulp.series(["build:widget:source"]), () =>
 {
     return gulp.src("node_modules/vss-web-extension-sdk/lib/**/*.*",{ base: "node_modules/vss-web-extension-sdk" })
     .pipe(gulp.dest(`${paths.outputPath}widgets/ProjectStatus`));
 });
 
-gulp.task("build:widget:source", [], () => {
-    return gulp.src(`${paths.sourceRoot}widgets/**/*`, {base: `${paths.sourceRoot}widgets`})
-    .pipe(gulp.dest(`${paths.outputPath}widgets`));
-});
-
-gulp.task("build:copy", [], () => {
+gulp.task("build:copy", () => {
     return gulp.src([`${paths.sourceRoot}*.*`, `${paths.sourceRoot}img/**/*.*`], { base: `${paths.sourceRoot}`})
     .pipe(gulp.dest("dist"));
 });
 
-gulp.task("build:copy:task:content", ["build:tasks"], (cb) => {
+gulp.task("build:copy:task:content", gulp.series(["build:tasks"]), (cb) => {
     var stream = gulp.src(`${paths.sourceRoot}tasks/**/*.{json,png,svg,zip,gz}`, { base: paths.sourceRoot}).pipe(gulp.dest(paths.outputPath));
+    return stream;
 });
 
-gulp.task("build:copy:externals", ["build:tasks"], (cb) => {
+gulp.task("build:copy:externals", gulp.series(["build:tasks"]), (cb) => {
     var stream = gulp.src(paths.externals.map(x => `node_modules/${x}/**/*.*`), { base: "."});
 
     glob(`${paths.outputPath}/tasks/**/task.json`, (err, matches) => {
@@ -56,9 +57,23 @@ gulp.task("build:copy:externals", ["build:tasks"], (cb) => {
 
         stream.on("end", cb);
     });
+
+    return stream;
 });
 
-gulp.task("clean", [], () => {
-    return gulp.src("dist", {read: false})
+gulp.task("clean", () => {
+    return gulp.src("dist", {read: false, allowEmpty: true})
     .pipe(clean());
 });
+
+gulp.task("build",
+    gulp.series(
+        "clean",
+        gulp.parallel(
+            "build:tasks",
+            "build:widgets",
+            "build:copy",
+            "build:copy:task:content"
+        )
+    )
+);
