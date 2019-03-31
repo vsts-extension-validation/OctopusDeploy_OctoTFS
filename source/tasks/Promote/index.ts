5156759b85fb12710bcc04214fc90f53d44ad467
Rob Pearson <rob.pearson@me.com>
2019-03-31 22:18:25 +1000

    Moving tasks to support old Space field and new one. Testing. WIP.

--- source/tasks/Promote/index.ts
+++ source/tasks/Promote/index.ts
@@ -18,26 +18,41 @@ async function run() {
         let project;
         let from;
         let to;
-        let deploymentForTenants;
+        let deployForTenants;
         let deployForTenantTags;
         const showProgress = tasks.getBoolInput("ShowProgress");
         const additionalArguments = tasks.getInput("AdditionalArguments");
 
-        if (hasSpaces) {
-            const spaceId = tasks.getInput("SpaceId", true);
+        const legacySpaceString = tasks.getInput("Space");
+        let spaceId = tasks.getInput("SpaceId");
+        const hasLegacySpace = legacySpaceString && legacySpaceString.length > 0;
+        const hasModernSpace = hasSpaces && (spaceId && spaceId.length > 0);
+
+        if (legacySpaceString && !hasModernSpace) {
+            // Use legacy value - Override space and use non-space related project, channel etc
+            space = legacySpaceString;
+            project = await utils.resolveProjectName(connection, tasks.getInput("Project", true)).then(x => x.value);
+            from = tasks.getInput("From", true);
+            to = utils.getRequiredCsvInput("To");
+            deployForTenants = utils.getOptionalCsvInput("DeployForTenants");
+            deployForTenantTags= utils.getOptionalCsvInput("DeployForTenantTags");
+        }
+        else if ((hasLegacySpace && hasModernSpace) || (!legacySpaceString && hasModernSpace)) {
+            // Ignore legacy value and new modern values
             space = await utils.resolveSpaceName(connection, spaceId).then(x => x.value);
             project = await utils.resolveProjectNameInSpace(connection, spaceId, tasks.getInput("ProjectNameInSpace", true)).then(x => x.value);
             from = tasks.getInput("FromEnvironmentInSpace", true);
             to = utils.getRequiredCsvInput("ToEnvironmentsInSpace");
-            deploymentForTenants = utils.getOptionalCsvInput("DeployForTenantsInSpace");
+            deployForTenants = utils.getOptionalCsvInput("DeployForTenantsInSpace");
             deployForTenantTags = utils.getOptionalCsvInput("DeployForTenantTagsInSpace");
         }
         else {
+            // No Space or Default Space
             space = null;
             project = await utils.resolveProjectName(connection, tasks.getInput("Project", true)).then(x => x.value);
             from = tasks.getInput("From", true);
             to = utils.getRequiredCsvInput("To");
-            deploymentForTenants = utils.getOptionalCsvInput("DeployForTenants");
+            deployForTenants = utils.getOptionalCsvInput("DeployForTenants");
             deployForTenantTags= utils.getOptionalCsvInput("DeployForTenantTags");
         }
 
@@ -49,7 +64,7 @@ async function run() {
             connectionArguments(connection),
             argumentEnquote("from", from),
             multiArgument(argumentEnquote, "to", to),
-            multiArgument(argumentEnquote, "tenant", deploymentForTenants),
+            multiArgument(argumentEnquote, "tenant", deployForTenants),
             multiArgument(argumentEnquote, "tenanttag", deployForTenantTags),
             flag("progress", showProgress),
             includeArguments(additionalArguments)
