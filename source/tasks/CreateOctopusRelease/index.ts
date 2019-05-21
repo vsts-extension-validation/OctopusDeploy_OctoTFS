16de162909ea3bf7b2e84c09a1a6d324e79b8fe7
Rob Pearson <rob.pearson@me.com>
2019-05-21 15:10:10 +1000

    Revert "Update Space field to be a picklist. Removed cruft."
    This reverts commit 169d038c8ced4b46905dc09ca2b8bc31305a4584.

--- source/tasks/CreateOctopusRelease/index.ts
+++ source/tasks/CreateOctopusRelease/index.ts
@@ -15,18 +15,56 @@ async function run() {
         const vstsConnection = utils.createVstsConnection(environmentVariables);
         const connection = utils.getDefaultOctopusConnectionDetailsOrThrow();
 
-        const space = tasks.getInput("Space");
-        const project = await utils.resolveProjectName(connection, tasks.getInput("ProjectName", true)).then(x => x.value);
+        const hasSpaces = tasks.getBoolInput("HasSpaces");
+
+        let space;
+        let project;
         const releaseNumber = tasks.getInput("ReleaseNumber");
-        const channel = tasks.getInput("Channel");
+        let channel;
         const changesetCommentReleaseNotes = tasks.getBoolInput("ChangesetCommentReleaseNotes");
         const workItemReleaseNotes = tasks.getBoolInput("WorkItemReleaseNotes");
         const customReleaseNotes = tasks.getInput("CustomReleaseNotes");
-        const deployToEnvironments = utils.getOptionalCsvInput("DeployToEnvironment");
-        const deployForTenants = utils.getOptionalCsvInput("DeployForTenants");
-        const deployForTenantTags = utils.getOptionalCsvInput("DeployForTenantTags");
-        const deploymentProgress = tasks.getBoolInput("DeploymentProgress");
+        let deployToEnvironments;
+        let deployForTenants;
+        let deployForTenantTags;
+        let deploymentProgress;
         const additionalArguments = tasks.getInput("AdditionalArguments");
+
+        const legacySpaceString = tasks.getInput("Space");
+        let spaceId = tasks.getInput("SpaceId");
+        const hasLegacySpace = legacySpaceString && legacySpaceString.length > 0;
+        const hasModernSpace = hasSpaces && (spaceId && spaceId.length > 0);
+
+        if (legacySpaceString && !hasModernSpace) {
+            // Use legacy value - Override space and use non-space related project, channel etc
+            space = legacySpaceString;
+            project = await utils.resolveProjectName(connection, tasks.getInput("ProjectName", true)).then(x => x.value);
+            channel = tasks.getInput("Channel");
+            deployToEnvironments = utils.getOptionalCsvInput("DeployToEnvironment");
+            deployForTenants = utils.getOptionalCsvInput("DeployForTenants");
+            deployForTenantTags = utils.getOptionalCsvInput("DeployForTenantTags");
+            deploymentProgress = tasks.getBoolInput("DeploymentProgress");
+        }
+        else if ((hasLegacySpace && hasModernSpace) || (!legacySpaceString && hasModernSpace)) {
+            // Ignore legacy value and new modern values
+            space = await utils.resolveSpaceName(connection, spaceId).then(x => x.value);
+            project = await utils.resolveProjectNameInSpace(connection, spaceId, tasks.getInput("ProjectNameInSpace", true)).then(x => x.value);
+            channel = tasks.getInput("ChannelInSpace");
+            deployToEnvironments = utils.getOptionalCsvInput("DeployToEnvironmentInSpace");
+            deployForTenants = utils.getOptionalCsvInput("DeployForTenantsInSpace");
+            deployForTenantTags = utils.getOptionalCsvInput("DeployForTenantTagsInSpace");
+            deploymentProgress = tasks.getBoolInput("DeploymentProgressInSpace");
+        }
+        else {
+            // No Space or Default Space
+            space = null;
+            project = await utils.resolveProjectName(connection, tasks.getInput("ProjectName", true)).then(x => x.value);
+            channel = tasks.getInput("Channel");
+            deployToEnvironments = utils.getOptionalCsvInput("DeployToEnvironment");
+            deployForTenants = utils.getOptionalCsvInput("DeployForTenants");
+            deployForTenantTags = utils.getOptionalCsvInput("DeployForTenantTags");
+            deploymentProgress = tasks.getBoolInput("DeploymentProgress");
+        }
 
         const octo = await utils.getOrInstallOctoCommandRunner("create-release");
 
