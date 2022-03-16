7d7bde5072c87b51c4f3c18885181449f2fe2335
John Simons <john.simons@octopus.com>
2022-03-16 14:02:11 +1000

    Adding jest reporter

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ const config: Config.InitialOptions = {
     maxWorkers: 4,
     verbose: true,
     preset: "ts-jest/presets/js-with-ts",
+    runner: "jest-runner-eslint",
 };
 
 export default config;
